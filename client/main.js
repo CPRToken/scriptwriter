332f8449decae72e27636f37946b78727bae4436
CPRToken <99814986+CPRToken@users.noreply.github.com>
2023-03-01 13:10:17 +0700

    first commit

---
+++ client/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $('#form').submit(function (event) {
+    event.preventDefault();
+    const prompt = $('#prompt').val();
+    const temperature = $('#temperature').val();
+    const max_tokens = $('#max_tokens').val();
+    
+    $.ajax({
+      type: 'POST',
+      url: '/api/completions',
+      data: {
+        prompt: prompt,
+        temperature: temperature,
+        max_tokens: max_tokens
+      },
+      success: function(response) {
+        document.getElementById("response").innerHTML = response.result.replace(/\n/g, '<br />');
+      }
+      
+    });
+  });
+});
